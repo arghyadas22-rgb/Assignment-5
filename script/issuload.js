184ee119482083b30303f73d29b109ea40721b45
arghyadas22-rgb <arghya1842004@gmail.com>
2026-03-08 11:12:28 +0600

    all button working and add all button js

--- script/issuload.js
+++ script/issuload.js
@@ -3,24 +3,41 @@ console.log("Hello world")
 const cardcontainer = document.getElementById("card-container");
 
 
+
+
+
 async function loadcard() {
     const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
     const data = await res.json();
+    allIssues = data.data;
     displaycard(data.data);
 }
 
 function displaycard(issues){
-    console.log(issues);
+    cardcontainer.innerHTML = "";
     issues.forEach(card => {
         console.log(card);
+
+    const badges = card.labels.map(label => {
+        return `<div class="badge badge-soft bg-[#ef44441c] text-[#EF4444] px-4 py-3.5 font-medium">
+            ${label}
+        </div>`;
+    }).join("");
 
         // hlep form google just status icon
         const statusIcon = card.status === "closed" 
             ? "./assets/Closed- Status .png"
             : "./assets/Open-Status.png";
 
+
+            // border color
+            const borderColor = card.status === "closed"
+            ? "border-[#A855F7]"
+            : "border-[#00A96E]";
+
+
         const allcard = document.createElement("div")
-        allcard.className = "card bg-base-100 shadow-sm p-4 border-t-4 border-[#00A96E]";
+        allcard.className = `card bg-base-100 shadow-sm p-4 border-t-4 ${borderColor} flex flex-col justify-between`;
         allcard.innerHTML = `<div class="flex justify-between mb-3">
                         <img class="w-6 h-6" src="${statusIcon}" alt="">
                         <span class="${card.priority === 'high' ? 'bg-red-100 text-red-500' :card.priority === 'medium' ? 'bg-yellow-100 text-yellow-500' :'bg-gray-100 text-gray-500'}    bg-[#ef44441c] text-[#EF4444] px-6 py-1 rounded-[50px]">${card.priority}</span>
@@ -29,10 +46,8 @@ function displaycard(issues){
                         <h2 class="font-semibold text-[1.25rem] text-[#1F2937] pb-2">${card.title}</h2>
                         <p class="text-[#64748B] line-clamp-2">${card.description}</p>
                     </div>
-                    <div class="mb-3">
-                        <div class="badge badge-soft bg-[#ef44441c] text-[#EF4444] px-4 py-3.5 font-medium"><span><img class="w-3" src="./assets/Vector.png" alt=""></span>BUG</div>
-
-                        <div class="badge badge-soft bg-[#fde68a59] text-[#D97706] px-4 py-3.5 font-medium"><span><img class="w-3" src="./assets/Vector (1).png" alt=""></span>HELP WANTED</div>
+                    <div class="mb-3 flex gap-2">
+                        ${badges}
                     </div>
 
                     <div class="border border-gray-200 w-full">
@@ -51,5 +66,37 @@ function displaycard(issues){
         cardcontainer.appendChild(allcard);
     });
 }
+
+
+
+const allBtn = document.getElementById("all-btn");
+const openBtn = document.getElementById("open-btn");
+const closedBtn = document.getElementById("closed-btn");
+
+allBtn.addEventListener("click", ()=> {
+    displaycard(allIssues);
+})
+
+openBtn.addEventListener("click", ()=> {
+    const openIssues = allIssues.filter(issue => issue.status === "open");
+    displaycard(openIssues);
+})
+
+closedBtn.addEventListener("click", ()=> {
+    const closedIssues = allIssues.filter(issue => issue.status === "closed");
+    displaycard(closedIssues);;
+})
+
+
+const buttons = document.querySelectorAll(".filter-btn");
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        buttons.forEach(b => b.classList.remove("btn-primary"));
+        btn.classList.add("btn-primary");
+    });
+});
+
+
 
 loadcard();
